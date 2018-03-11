bf70257c6f5c9286c5bcbcd8215b8e6731b1735c
kingabiczysko <kinga.biczysko@gmail.com>
2018-03-11 23:02:40 +0100

    Modul 21.2

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,9 +1,9 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
-import com.crud.tasks.service.TrelloService;
+import com.crud.tasks.trello.facade.TrelloFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +16,19 @@ public class TrelloController {
 
     @Autowired
     //private TrelloClient trelloClient;
-    private TrelloService trelloService;
+    //private TrelloService trelloService;
+    private TrelloFacade trelloFacade;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
             public List<TrelloBoardDto> getTrelloBoards() {
-        return trelloService.fetchTrelloBoards();
+        return trelloFacade.fetchTrelloBoards();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
+    public CreatedTrelloCardDto createTrelloCard(@RequestBody TrelloCardDto trelloCardDto){
+        return trelloFacade.createCard(trelloCardDto);
+    }
+}
 
 
 //
@@ -42,14 +50,3 @@ public class TrelloController {
 //                .filter(e -> e.getName().contains("Kodilla"))
 //                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId()
 //        + " " + trelloBoardDto.getName()));
-
-
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
-    public CreatedTrelloCard createTrelloCard(@RequestBody
-    TrelloCardDto trelloCardDto){
-
-        return trelloService.createTrelloCard(trelloCardDto);
-    }
-}
