24c467213106b0f01cfed78854d473259c2ea3ee
kingabiczysko <kinga.biczysko@gmail.com>
2018-03-23 10:11:33 +0100

    Zadanie 24.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.trello.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -10,12 +11,23 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        return templateEngine.process("mail/created-trello-card-mail", context);
+        context.setVariable("task_url", "http://localhost:8888/tasks_frontend");
+        context.setVariable("button", "Visit webside");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbyeMessage", "Bye, see you at the next message :)");
+        context.setVariable("company_details", adminConfig.getCompanyName() + ", "
+                + adminConfig.getCompanyStreet() + " " + adminConfig.getCompanyStreetNumber() +
+                ", " + adminConfig.getCompanyTown() + ", " + adminConfig.getCompanyPhone());
+        return templateEngine.process("mail/created-trello-card-mail2", context);
     }
 }
--- src/main/java/com/crud/tasks/trello/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/trello/config/AdminConfig.java
@@ -7,6 +7,25 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class AdminConfig {
+
+    @Value("${admin.name}")
+    private String adminName;
+
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.app.administrator.address.street}")
+    private String companyStreet;
+
+    @Value("${info.app.administrator.address.number}")
+    private String companyStreetNumber;
+
+    @Value("${info.app.administrator.address.town}")
+    private String companyTown;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
 }
